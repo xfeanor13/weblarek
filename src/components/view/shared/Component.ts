86e5638204a8020225359ad62793d321f71670f7
Anton Tomashevskiy <anton.tomashevskiy@yandex.ru>
2025-10-22 23:38:31 +0300

    Add files via upload

---
+++ src/components/view/shared/Component.ts
@@ -0,0 +1,8 @@
+export abstract class Component<T> {
+	constructor(protected readonly container: HTMLElement) {}
+
+	render(data?: Partial<T>): HTMLElement {
+		Object.assign(this as object, data ?? {});
+		return this.container;
+	}
+}
