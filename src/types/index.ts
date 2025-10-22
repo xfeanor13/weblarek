bcbbac0323dad8f3a19debd615af99fc5230911a
Anton Tomashevskiy <anton.tomashevskiy@yandex.ru>
2025-10-22 23:46:30 +0300

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,42 +1,107 @@
-// интерфейс api
-export interface IDataApi {
-	getProducts(): Promise<IProduct[]>;
-	getProduct(id: string): Promise<IProduct>;
-	sendOrder(data: IOrder): Promise<TOrderResult>;
-}
+// Типы данных
 
-// тип оплаты
-export type PaymentType = 'online' | 'personally';
+// Способы оплаты
+export type TUserPayment = 'card' | 'cash' | '';
 
-// данные товара
-export interface IProduct {
+// Запросы к серверу
+export type ApiPostMethods = 'POST' | 'GET';
+
+// Типы данных карточки, которые передаются в render
+export type CardBaseProps  = { title: string; price: number | null };
+export type CardMediaProps = { category?: string; image?: string; index?: number; buttonDisable?: boolean};
+export type CardsArrayProps ={ items: ICard[]};
+export type CardProps = CardBaseProps & CardMediaProps;
+
+// Тип источника события изменения корзины
+
+export type TBasketSource = 'preview' | 'basket';
+
+// Интерфейс карточки
+export interface ICard {
 	id: string;
-	description: string;
-	image: string;
 	title: string;
+	image: string;
 	category: string;
-	price: number | null; // есть товар, у которого нет цены
+	price: number | null;
+	description: string;
 }
 
-// данные заказа
-export interface IOrder {
-	payment: PaymentType;
+// Данные покупателя
+export interface IUser {
+	payment: TUserPayment
 	address: string;
 	email: string;
 	phone: string;
-	items: string[];
-	total: number;
 }
 
-// интерфейс корзины
-export interface IBasket {
-	items: string[];
+// Данные заказа
+export interface IOrder extends IUser {
 	total: number;
+	items: string[];
 }
 
-export type TOrderResult = {
+// Ответ сервера на успешную отправку заказа
+export interface IApiOrderResponse {
 	id: string;
 	total: number;
-};
+}
 
-export type TOrderForm = Omit<IOrder, 'total' | 'items'>;
+// Интерфейс класса данных каталога карточек
+export interface ICardsData {
+	setItems (items: ICard[]): void; 
+	getItems(): ICard[]; 
+	getItem(id: string): ICard; 
+}
+
+// Базовый интерфейс карточки товара
+export interface ICardBaseView {
+	set title (text: string);
+	set price (price: number);
+	set id (id: string);
+}
+
+// Расширенный интерфейс карточки товара
+export interface ICardFullView extends ICardBaseView {
+	set category (text: string);
+	set image (link: string);
+}
+
+// Интерфейс каталога карточек на главной странице
+export interface ICatalogView {
+	set content (items: HTMLElement[]); 
+	set locked(isOpen: boolean);
+}
+
+// Интерфейс модального окна
+export interface IModalView {
+	openModal(): void;
+	closeModal(): void;
+	set content (element: HTMLElement);
+}
+
+// Интерфейс содержимого модального окна с корзиной
+export interface IBasketView {
+	totalPriceElement: HTMLElement;
+	submitButton: HTMLButtonElement;
+	set submitButtonText (text: string);
+	set totalPrice (totalPrice: number);
+}
+
+// Родительский интерфейс формы
+export interface IBaseFormView {
+	resetForm(): void;
+}
+
+// Интерфейс окна с подтверждением оформления заказа
+export interface IOrderSuccessView {
+	totalPriceElement: HTMLElement;
+	successCloseButton: HTMLButtonElement;
+	set totalPrice (totalPrice: number);
+}
+
+// Интерфейс класса для обращения к серверу
+export interface IApi {
+	baseUrl: string;
+	get<T>(uri: string): Promise<T>;
+	post<T>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+}
